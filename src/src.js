2409e5000c83ef4100f0d3a9a2066c95d8aae0c6
haseeb bhatti <haseebhatti@gmail.com>
2016-08-08 13:21:50 -0400

    uploading update files

--- src/src.js
+++ src/src.js
@@ -1,34 +1,82 @@
-  var RomanNumerals = {
-      convert: function(num) {
+<!-- jQuery -->
+<
+script src = "js/jquery.js" > < /script>
 
-          if (num == 2)
-              return "II"
-          if (num == 3)
-              return "III"
-          if (num == 0)
-              return "0"
-          if (num == 5)
-              return "V"
-          return "I"
-      }
+<!-- Bootstrap Core JavaScript -->
+<
+script src = "js/bootstrap.min.js" > < /script>
 
-  };
-
-  //(function(){
-  // 'use strict';
-  //     featureSteps(/Roman numerals/)
-  //         .given(/I have a Roman numerals calculator/, function(){
-  //             this.calculator = new Calculator();
-  //         })
-  //         .given(/I enter '(.*)' in the calculator/, function(roman){
-  //             this.calculator.setInput(roman);
-  //         })
-  //         .when(/I convert the roman numeral/, function(){
-  //             this.calculator.convert();
-  //         })
-  //         .when(/I press add/, function(){
-  //             this.calculator.add();
-  //         })
-  //         .then(/the displayed value is '(.*)'/, function(num){
-  //             expect(this.calculator.getDisplayedValue()).toBe(num);
-  //         })();
+<!-- Custom Theme JavaScript -->
+<
+script >
+    // Closes the sidebar menu
+    $("#menu-close").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+// Opens the sidebar menu
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+// Scrolls to the selected menu item on the page
+$(function() {
+    $('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+//#to-top button appears after scrolling
+var fixed = false;
+$(document).scroll(function() {
+    if ($(this).scrollTop() > 250) {
+        if (!fixed) {
+            fixed = true;
+            // $('#to-top').css({position:'fixed', display:'block'});
+            $('#to-top').show("slow", function() {
+                $('#to-top').css({
+                    position: 'fixed',
+                    display: 'block'
+                });
+            });
+        }
+    } else {
+        if (fixed) {
+            fixed = false;
+            $('#to-top').hide("slow", function() {
+                $('#to-top').css({
+                    display: 'none'
+                });
+            });
+        }
+    }
+});
+// Disable Google Maps scrolling
+// See http://stackoverflow.com/a/25904582/1607849
+// Disable scroll zooming and bind back the click event
+var onMapMouseleaveHandler = function(event) {
+    var that = $(this);
+    that.on('click', onMapClickHandler);
+    that.off('mouseleave', onMapMouseleaveHandler);
+    that.find('iframe').css("pointer-events", "none");
+}
+var onMapClickHandler = function(event) {
+        var that = $(this);
+        // Disable the click handler until the user leaves the map area
+        that.off('click', onMapClickHandler);
+        // Enable scrolling zoom
+        that.find('iframe').css("pointer-events", "auto");
+        // Handle the mouse leave event
+        that.on('mouseleave', onMapMouseleaveHandler);
+    }
+    // Enable map zooming with mouse scroll when the user clicks the map
+$('.map').on('click', onMapClickHandler); <
+/script>
